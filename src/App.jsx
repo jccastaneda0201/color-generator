f5c0f5d0f108833557b07e328358ffad191b4d8b
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-07-14 16:33:45 +0200

    feat: add color list and form components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 import ColorList from './ColorList';
 import Form from './Form';
 import Values from 'values.js';
 
 const App = () => {
   const [colors, setColors] = useState(new Values('#f15025').all(10));
+  // toast.error('error');
+  // toast.success('success');
   return (
     <main>
       <Form />
       <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
     </main>
   );
 };
