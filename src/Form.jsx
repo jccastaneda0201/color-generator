ea46113abc2d08a0d5dce01910ba259d2e9573a4
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-07-14 16:59:17 +0200

    feat: add color list and form components

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 
-const Form = () => {
+const Form = ({ addColor }) => {
   const [color, setColor] = useState('');
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addColor(color);
+  };
   return (
     <section className="container">
-      <h4>Color generator</h4>
+      <h4>color generator</h4>
       <form className="color-form" onSubmit={handleSubmit}>
         <input type="color" value={color} onChange={(e) => setColor(e.target.value)} />
         <input
@@ -14,12 +17,11 @@ const Form = () => {
           onChange={(e) => setColor(e.target.value)}
           placeholder="#f15025"
         />
-        <button type="submit" className="btn" style={{ background: color }}>
-          generate
+        <button className="btn" type="submit" style={{ background: color }}>
+          submit
         </button>
       </form>
     </section>
   );
 };
-
 export default Form;
--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,9 +1,24 @@
+import { toast } from 'react-toastify';
+
 const SingleColor = ({ color, index }) => {
   const { hex, weight } = color;
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success('color copied to clipboard');
+      } catch (error) {
+        toast.error('failed to copy color');
+      }
+    } else {
+      toast.error('clipboard not supported');
+    }
+  };
   return (
     <article
       className={index > 10 ? 'color color-light' : 'color'}
       style={{ background: `#${hex}` }}
+      onClick={saveToClipboard}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">#{hex}</p>
